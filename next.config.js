2339b01fd8d75959d793df20cb5a7c5436ef044c
Airel Camilo Khairan <airel.camilo@ui.ac.id>
2023-08-26 22:29:48 +0700

    Fix deployment

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        outputFileTracingIgnores: ["**canvas**"],
+    },
     webpack: (config) => {
         config.externals.push({ sharp: 'commonjs sharp', canvas: 'commonjs canvas' })
         return config
-    },
-    experimental: {
-        outputFileTracingRoot: path.join(__dirname, '../../'),
-        outputFileTracingExcludes: {
-            '*': [
-                'node_modules/canvas',
-            ],
-        },
     },
 }
 
