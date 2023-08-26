e6d071529441fa7796bbabfee1eb6e9b38bb9c1e
Airel Camilo Khairan <airel.camilo@ui.ac.id>
2023-08-26 22:20:37 +0700

    Fix deployment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
     webpack: (config) => {
         config.externals.push({ sharp: 'commonjs sharp', canvas: 'commonjs canvas' })
         return config
-    }
+    },
+    experimental: {
+        outputFileTracingRoot: path.join(__dirname, '../../'),
+        outputFileTracingExcludes: {
+            '*': [
+                'node_modules/canvas',
+            ],
+        },
+    },
 }
 
 module.exports = nextConfig
